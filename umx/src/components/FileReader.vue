0dc1afd97d299059a9f4d080a852050136b3740f
Clara Jacintho <mcmachry@gmail.com>
2019-12-16 10:51:56 +0100

    removed music files

--- umx/src/components/FileReader.vue
+++ umx/src/components/FileReader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <input type="file" id="audioFile" @change="readFile">
+    <input type="file" id="audioFile" @change="loadFile">
     <h3>Pre-preprocessing audio</h3>
     <audio id="ogAudio" controls></audio>
   </div>
@@ -11,17 +11,29 @@ export default {
   name: 'FileReader',
 
   methods: {
-    readFile () {
+    loadFile () {
       let blob = window.URL || window.webkitURL
-      let file = this.files[0]
+      let file = document.getElementById('audioFile').files[0]
       let fileURL = blob.createObjectURL(file)
       let ogAudio = document.getElementById('ogAudio') // is... this.. how you do this in Vue? I dont think so
       ogAudio.src = fileURL
-      // do the same thing as Koekestra
-      console.log(typeof fileURL)
-      console.log('test')
-      //  readFile(file)
+    },
+
+    readFile(file) {
+      const reader = new FileReader();
+      reader.onerror = function() {
+          console.log('An error occurred when reading the file. Code: ' + reader.error.code) 
+      };
+
+      reader.onload = function() {
+          const arrayBuffer = reader.result; 
+          console.log(file.name + "The file was loaded sucessfully. Decoding...")
+          decode(file.name, arrayBuffer);
+      };
+
+      reader.readAsArrayBuffer(file)
     }
+
   }
 
 }
