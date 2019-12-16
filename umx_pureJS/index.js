419548e6f9a91f090485dbacf6404cc9b8fa0dc4
Delton <delton.vaz@hotmail.com>
2019-12-16 16:53:46 +0100

    started stft-inverse transformations and MSE calculations

--- umx_pureJS/index.js
+++ umx_pureJS/index.js
@@ -1,12 +1,7 @@
 //'use strict';
 const fs = require('fs');
 let wav = require('node-wav');
-var shortTimeFT = require('stft'); //(1, 4096, onFreq); //4096 = FFT_SIZE
-var babar = require('babar');
-//var stftOld = require('./stft');
-// var KissFFT = require('kissfft-js');
-//
-// var A2_1024 = require('./audioBuffer');
+let shortTimeFT = require('stft');
 
 
 const FFT_SIZE = 4096;
@@ -160,154 +155,34 @@ function readFile(file) {
 
 }
 
+function stftPassThru(frame_size, input) {
+    var stft = STFT(1, frame_size, onfft)
+    var istft = STFT(-1, frame_size, onifft)
+    var output = new Float32Array(input.length)
+    var in_ptr = 0
+    var out_ptr = 0
 
-/**
- * STTFT old
- * @param frame_size
- * @returns {[]}
- */
-
-function initWindow(frame_size) {
-    const win = [];
-    for (var i = 0; i < frame_size; i++) {
-        win[i] = 0.5 - 0.5 * Math.cos(2.0 * Math.PI * i / frame_size);
+    function onfft(x, y) {
+        istft(x, y)
     }
-    return win;
+
+    function onifft(v) {
+        console.log(Array.prototype.slice.call(v))
+        for(var i=0; i<v.length; ++i) {
+            output[out_ptr++] = v[i]
+        }
+    }
+
+    for(var i=0; i+frame_size<=input.length; i+=frame_size) {
+        stft(input.subarray(i, i+frame_size))
+    }
+    stft(new Float32Array(frame_size))
+    return output
 }
 
-// http://yukara-13.hatenablog.com/entry/2013/11/17/210204
-function mySTFT(src, frame_size, hop_size, proc_frames) {
-    const win = initWindow(frame_size);  // hann窓を作る
-    const out = new Array();
-    const signal = new Float32Array(frame_size*2);
-
-    // proc_frames 2560
-    for (var i = 0; i < proc_frames; i++) {
-        const start = hop_size * i - frame_size / 2;
-        if (start > src.length) {
-            out.push(new Float32Array((frame_size / 2 + 1) * 2)); // ((4096/2)+1)*2) = 4098
-            continue;
-        } else if (start < 0) {
-            for (var j = 0; j < frame_size; j++) {
-                signal[j*2+0] = src[Math.abs(start + j)] * win[j]; // reflect
-                signal[j*2+1] = 0;
-            }
-        } else if (start + frame_size > src.length) {
-            for (var j = 0; j < frame_size; j++) {
-                const exc = (start + j) - src.length;
-                signal[j*2+0] = ((start + j < src.length) ? src[start + j] : src[src.length - 1 - exc]) * win[j]; // reflect
-                signal[j*2+1] = 0;
-            }
-        } else {
-            for (var j = 0; j < frame_size; j++) {
-                signal[j*2+0] = src[start + j] * win[j];
-                signal[j*2+1] = 0;
-            }
-        }
-
-        const phasors = myFFT(signal);
-        const half = new Float32Array((frame_size / 2 + 1)*2);
-        for (var j = 0; j < frame_size / 2 + 1; j++) {
-            half[j*2+0] = phasors[j*2+0]; //Real
-            half[j*2+1] = phasors[j*2+1]; //Im
-        }
-        out.push(half);
-    }
-    return out;
-}
-
-function myISTFT(dst, mag, phase, frame_size, hop_size, proc_frames) {
-    const win = initWindow(frame_size);  // hann窓を作る
-    const fphasors = new Float32Array(frame_size*2);
-    for (var i = 0; i < proc_frames; i++) {
-        const start = hop_size * i - frame_size / 2;
-        if (start > dst.length) {
-            return;
-        }
-        const fmag   = mag  [i];
-        const fphase = phase[i];
-
-        for (var j = 0; j < frame_size / 2 + 1; j++) {
-            fphasors[j*2+0] = Math.cos(fphase[j]) * fmag[j];
-            fphasors[j*2+1] = Math.sin(fphase[j]) * fmag[j];
-        }
-        for (var j = 0; j < frame_size / 2 - 1; j++) {
-            fphasors[(frame_size - 1 - j)*2+0] =  fphasors[(1 + j)*2+0];
-            fphasors[(frame_size - 1 - j)*2+1] = -fphasors[(1 + j)*2+1];
-        }
-
-        const signal = myIFFT(fphasors);
-
-        if (start < 0) {
-            for (var j = -start; j < frame_size; j++) {
-                dst[start + j] += signal[j*2+0] * win[j];
-            }
-        } else if (start + frame_size > dst.length) {
-            for (var j = 0; j < frame_size && start + j < dst.length; j++) {
-                dst[start + j] += signal[j*2+0] * win[j];
-            }
-        } else {
-            for (var j = 0; j < frame_size; j++) {
-                dst[start + j] += signal[j*2+0] * win[j];
-            }
-        }
-    }
-}
-
-// http://www.kurims.kyoto-u.ac.jp/~ooura/fftman/ftmn1_2.html
-function myFFT(a){
-    const n = a.length / 2;
-    const theta = -2 * Math.PI / n;
-    /* ---- scrambler ---- */
-    var i = 0;
-    for (var j = 1; j < n - 1; j++) {
-        for (var k = n >> 1; k > (i ^= k); k >>= 1);
-        if (j < i) {
-            var xr = a[j*2+0];
-            var xi = a[j*2+1];
-            a[j*2+0] = a[i*2+0];
-            a[j*2+1] = a[i*2+1];
-            a[i*2+0] = xr;
-            a[i*2+1] = xi;
-        }
-    }
-    var m;
-    for (var mh = 1; (m = mh << 1) <= n; mh = m) {
-        var irev = 0;
-        for (i = 0; i < n; i += m) {
-            var wr = Math.cos(theta * irev);
-            var wi = Math.sin(theta * irev);
-            for (var k = n >> 2; k > (irev ^= k); k >>= 1);
-            for (var j = i; j < mh + i; j++) {
-                k = j + mh;
-                xr = a[j*2+0] - a[k*2+0];
-                xi = a[j*2+1] - a[k*2+1];
-                a[j*2+0] += a[k*2+0];
-                a[j*2+1] += a[k*2+1];
-                a[k*2+0] = wr * xr - wi * xi;
-                a[k*2+1] = wr * xi + wi * xr;
-            }
-        }
-    }
-    return a;
-}
-
-function myIFFT(a) {
-    const ca = new Float32Array(a.length);
-    for (var i = 0; i < a.length / 2; i++) {
-        ca[i*2+0] =  a[i*2+0];
-        ca[i*2+1] = -a[i*2+1];
-    }
-    const ps = myFFT(ca);
-    const out = new Float32Array(ps.length);
-    for (var i = 0; i < ps.length / 2; i++) {
-        out[i*2+0] =  ps[i*2+0] / (ps.length / 2);
-        out[i*2+1] = -ps[i*2+1] / (ps.length / 2);
-    }
-    return out;
-}
-
-
+console.log(Array.prototype.slice.call(stftPassThru(8, new Float32Array([
+    0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+]))))
 
 console.log("teste")
-readFile("/Users/delton/pfe/ex1.wav")
+readFile("/Users/delton/pfe/audio_example.mp3")
--- umx_pureJS/utils/fourier.js
+++ umx_pureJS/utils/fourier.js
@@ -5,100 +5,79 @@ const FFT_SIZE = 4096; // Frame length?
 const PATCH_LENGTH = 512; // Frame step? In py = 1024?
 const HOP_SIZE = 1024; //
 const SR = 44100; // Sampling rate
+const fs = require('fs');
+const decode = require('audio-decode');
+const buffer = require('audio-lena/mp3');
+var toWav = require('audiobuffer-to-wav')
+const WaveFile = require('wavefile');
+const mse = require('mse');
+
 
 const STFT = require("stft")
 
 function readFile(file) {
-    const reader = new FileReader();
-    reader.onerror = function() {
-        console.log('An error occurred when reading the file. Code: ' + reader.error.code); 
-    };
-
-    reader.onload = function() {
-        const arrayBuffer = reader.result; 
-        console.log(file.name + "The file was loaded sucessfully. Decoding..."); 
-        decode(file.name, arrayBuffer);
-    };
-
-    reader.readAsArrayBuffer(file);
+    let arrayBuffer = fs.readFileSync(file);
+    decodeFile(file, arrayBuffer)
 }
 
-function decode(fileName, arrayBuffer) {
-    const audioCtx = new AudioContext({"sampleRate":SR});
-    audioCtx.decodeAudioData(arrayBuffer,
-        function(decodedData) {
-            const source = audioCtx.createBufferSource();
-            source.buffer = decodedData;
-            if (source.buffer.numberOfChannels != 2) {
-                console.log("Only stereo data is supported.");
-                return;
+function decodeFile(fileName, arrayBuffer) {
+    decode(arrayBuffer, (err, audioBuffer) => {
+        try {
+            console.log(audioBuffer.duration)
+            let wavFile1 = stftPassThru(FFT_SIZE, audioBuffer._channelData[0])
+            let wavFile2 = stftPassThru(FFT_SIZE, audioBuffer._channelData[1])
+
+            let output = new Float32Array(wavFile1, wavFile2)
+
+            console.log(output.length)
+
+            let result = mse(output, audioBuffer._data); /* results in a calcuation of 5.3125 */
+            if (result !== 0) {
+                console.log('data sets are different by ' + result);
             }
 
-            resample(fileName, source);
-        },
-        function(e) {
-            console.log("An error occurred during decoding. The browser may not support the file format. Try converting to another format. :" + e.name + " " + e.message);
-        });
+            let wav = new WaveFile();
+            wav.fromScratch(2, audioBuffer.sampleRate, '32f',
+                output);
+
+            console.log(wav.chunkSize)
+            fs.writeFileSync('alface2.wav', wav.toBuffer());
+
+        } catch (e) {
+            console.log(e)
+            throw e;
+        }
+    });
 }
 
-function resample(fileName, input){
-    console.log(fileName)
-    console.log(input)
-
-    var stft = STFT(1, FFT_SIZE, onfft)
-   
-   
-    function onfft(re, im) {
-      console.log("real: " + re)
-      console.log("imaginary: " + im)
-    }
-
-    //apply on sampling? 
-    for(var i=0; i+FFT_SIZE<=input.length; i+=FFT_SIZE) {
-        stft(input.subarray(i, i+FFT_SIZE))
-    }
-}
-let blob = window.URL || window.webkitURL;
-if (!blob) {
-    console.log('Your browser does not support Blob URLs :(');       
-}
-
-const input = document.getElementById("audioFile");
-
-input.addEventListener("change", function () {
-    let file = this.files[0],
-    fileURL = blob.createObjectURL(file);
-    ogAudio = document.getElementById('ogAudio')
-    ogAudio.src = fileURL;
-    ogAudio.pause()
-    // do the same thing as Koekestra
-    console.log(typeof fileURL)
-    readFile(file)
-});
-
-// Mikola's test
 function stftPassThru(frame_size, input) {
     var stft = STFT(1, frame_size, onfft)
     var istft = STFT(-1, frame_size, onifft)
     var output = new Float32Array(input.length)
     var in_ptr = 0
     var out_ptr = 0
-   
+
     function onfft(re, im) {
-      console.log("real: " + re)
-      console.log("imaginary: " + im)
+        // console.log("real: " + re)
+        // console.log("imaginary: " + im)
+        istft(re, im)
     }
-    
+
     function onifft(v) {
-      console.log(Array.prototype.slice.call(v))
+      //console.log(Array.prototype.slice.call(v))
       for(var i=0; i<v.length; ++i) {
         output[out_ptr++] = v[i]
       }
     }
-    
+
     for(var i=0; i+frame_size<=input.length; i+=frame_size) {
       stft(input.subarray(i, i+frame_size))
     }
+
     stft(new Float32Array(frame_size))
+
     return output
 }
+
+
+readFile('/Users/delton/pfe/audio_example.mp3')
