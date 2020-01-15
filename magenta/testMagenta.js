f7c3469af1498f0374ddf610438f5076f1bed9c5
Delton <delton.vaz@hotmail.com>
2020-01-15 17:07:57 +0100

    extract only STFT from magenta for tests and compare results

---
+++ magenta/testMagenta.js
@@ -0,0 +1,199 @@
+/**
+ * Test file for STFT and ISTFT using @magenta/music
+ */
+
+const magenta = require('@magenta/music/node/core');
+const tf = require('@tensorflow/tfjs-node');
+const fs = require('fs');
+const mse = require('mse');
+const FFT = require('fft.js')
+
+const FRAME_LENGTH = 4096
+const FRAME_STEP = 512
+
+let channel0 = readFile('channel0');
+
+// inverse spec params:
+let inverse_params = {
+    hopLength: FRAME_STEP,
+    sampleRate: 44100,
+    winLength: FRAME_LENGTH
+}
+
+// stft params
+let stft_params = {
+    //hopLength: FRAME_STEP,
+    sampleRate: 44100
+    //winLength: FRAME_LENGTH
+}
+
+let calcStft = stft(channel0, stft_params);
+
+console.log(calcStft.length, calcStft[0].length)
+
+
+/**
+ *
+ * @param file
+ * @returns Float32Array with the values from file
+ */
+function readFile(file) {
+    let arrayBuffer = fs.readFileSync(file).toString('utf-8');
+    let textByLine = arrayBuffer.split(" ");
+
+    let floatArray = new Float32Array(textByLine.length - 1) // -1 cuz the last value is NaN
+
+    let stringToFloatArray = textByLine.map(function(c) {
+        return parseFloat(c).toPrecision(16);
+    });
+
+    stringToFloatArray = stringToFloatArray.slice(0, -1); // Remove the last element NaN
+
+    floatArray = stringToFloatArray;
+
+    return floatArray;
+}
+
+function hannWindow(length) {
+    const win = new Float32Array(length);
+    for (let i = 0; i < length; i++) {
+        win[i] = 0.5 * (1 - Math.cos(2 * Math.PI * i / (length - 1)));
+    }
+    return win;
+}
+
+// function padConstant(data: Float32Array, padding: number|number[]) {
+function padConstant(data, padding) {
+    let padLeft, padRight;
+    if (typeof (padding) === 'object') {
+        [padLeft, padRight] = padding;
+    } else {
+        padLeft = padRight = padding;
+    }
+    const out = new Float32Array(data.length + padLeft + padRight);
+    out.set(data, padLeft);
+    return out;
+}
+
+//export function padCenterToLength(data: Float32Array, length: number) {
+function padCenterToLength(data, length) {
+    // If data is longer than length, error!
+    if (data.length > length) {
+        throw new Error('Data is longer than length.');
+    }
+
+    const paddingLeft = Math.floor((length - data.length) / 2);
+    const paddingRight = length - data.length - paddingLeft;
+    return padConstant(data, [paddingLeft, paddingRight]);
+}
+
+// function padReflect(data: Float32Array, padding: number) {
+function padReflect(data, padding) {
+    const out = padConstant(data, padding);
+    for (let i = 0; i < padding; i++) {
+        // Pad the beginning with reflected values.
+        out[i] = out[2 * padding - i];
+        // Pad the end with reflected values.
+        out[out.length - i - 1] = out[out.length - 2 * padding + i - 1];
+    }
+    return out;
+}
+
+/**
+ * Given a timeseries, returns an array of timeseries that are windowed
+ * according to the params specified.
+ */
+//function frame(data: Float32Array, frameLength: number, hopLength: number): Float32Array[] {
+function frame(data, frameLength, hopLength) {
+    const bufferCount = Math.floor((data.length - frameLength) / hopLength) + 1;
+    const buffers = Array.from({length: bufferCount}, (x, i) => new Float32Array(frameLength));
+    for (let i = 0; i < bufferCount; i++) {
+        const ind = i * hopLength;
+        const buffer = data.slice(ind, ind + frameLength);
+        buffers[i].set(buffer);
+        // In the end, we will likely have an incomplete buffer, which we should
+        // just ignore.
+        if (buffer.length !== frameLength) {
+            continue;
+        }
+    }
+    return buffers;
+}
+
+// function fft(y: Float32Array) {
+function fft(y) {
+    const fft = new FFT(y.length);
+    const out = fft.createComplexArray();
+    const data = fft.toComplexArray(y);
+    fft.transform(out, data);
+    return out;
+}
+
+// export function applyWindow(buffer: Float32Array, win: Float32Array) {
+function applyWindow(buffer, win) {
+    if (buffer.length !== win.length) {
+        console.error(
+            `Buffer length ${buffer.length} != window length ${win.length}.`);
+        return null;
+    }
+
+    const out = new Float32Array(buffer.length);
+    for (let i = 0; i < buffer.length; i++) {
+        out[i] = win[i] * buffer[i];
+    }
+    return out;
+}
+
+
+//function stft(y: Float32Array, params: SpecParams): Float32Array[] {
+/**
+ *
+ * @param y
+ * @param params
+ *
+  sampleRate: number;
+  hopLength?: number;
+  winLength?: number;
+  nFft?: number;
+  nMels?: number;
+  power?: number;
+  fMin?: number;
+  fMax?: number;
+ * @returns {[]}
+ */
+function stft(y, params) {
+    const nFft = params.nFft || 2048;
+    const winLength = params.winLength || nFft;
+    const hopLength = params.hopLength || Math.floor(winLength / 4);
+
+    let fftWindow = hannWindow(winLength);
+
+    // Pad the window to be the size of nFft.
+    fftWindow = padCenterToLength(fftWindow, nFft);
+
+    // Pad the time series so that the frames are centered.
+    y = padReflect(y, Math.floor(nFft / 2));
+
+    // Window the time series.
+    const yFrames = frame(y, nFft, hopLength);
+    // Pre-allocate the STFT matrix.
+    const stftMatrix = [];
+
+    const width = yFrames.length;
+    const height = nFft + 2;
+    for (let i = 0; i < width; i++) {
+        // Each column is a Float32Array of size height.
+        const col = new Float32Array(height);
+        stftMatrix[i] = col;
+    }
+
+    for (let i = 0; i < width; i++) {
+        // Populate the STFT matrix.
+        const winBuffer = applyWindow(yFrames[i], fftWindow);
+        const col = fft(winBuffer);
+        stftMatrix[i].set(col.slice(0, height));
+    }
+
+    return stftMatrix;
+}
+
