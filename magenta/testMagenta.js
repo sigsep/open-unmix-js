b81fd892a335903355eba1fefe76e602b751321f
Clara Jacintho <mcmachry@gmail.com>
2020-01-17 12:47:10 +0100

    Cleaned up the magenta test, started testing saved_model loading

--- magenta/testMagenta.js
+++ magenta/testMagenta.js
@@ -7,7 +7,7 @@ const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs');
 const mse = require('mse');
 const FFT = require('fft.js');
-const WaveFile = require('wavefile');
+const wv = require('wavefile');
 const decode = require('audio-decode');
 
 const FRAME_LENGTH = 4096
@@ -16,10 +16,7 @@ const SAMPLE_RATE = 44100
 const NUMBER_OF_CHANNELS = 2
 const PATCH_LENGTH = 512
 
-let channel0 = readFile('channel0');
-let channel1 = readFile('channel1');
-
-// inverse spec params:
+// ISTT params:
 const ispecParams = {
     //nFFt: 4096,
     winLength: 2048, //2048
@@ -31,52 +28,44 @@ const ispecParams = {
 let arrayBuffer = fs.readFileSync("audio_example.mp3");
 decodeFile(arrayBuffer);
 
-//Crio o TF 1D
-const input0 = tf.tensor1d(channel0, 'float32') // Here there's a bug that makes the array loses precision
-const input1 = tf.tensor1d(channel1, 'float32') // Here there's a bug that makes the array loses precision
+let channel0 = readFile('channel0');
+let channel1 = readFile('channel1');
+
+console.log("\n------------------- Test with KoeKestra's Data ---------------------------\n")
+console.log("\nProcessing channel 0\n")
+const result0 = preprocessing(channel0)
+console.log("\nProcessing channel 1\n")
+const result1 = preprocessing(channel1)
 
 
-// STFT
-let result0 = tf.signal.stft(input0, FRAME_LENGTH, HOP_LENGTH);
-let result1 = tf.signal.stft(input1, FRAME_LENGTH, HOP_LENGTH);
-
-console.log("j")
-result0.print(true)
-return
-
-let real0 = tf.real(result0);
-let real1 = tf.real(result1);
-// console.log("Real:")
-// real.print(true);
-
-let imag0 = tf.imag(result0);
-let imag1 = tf.imag(result1);
-// console.log("Imag:")
-// imag.print(true);
-
-let reIm0 = interleaveReIm(real0, imag0);
-let reIm1 = interleaveReIm(real1, imag1);
-
-//console.log(reIm)
-
-let calcISTFT0 = istft(reIm0, ispecParams);
-let calcISTFT1 = istft(reIm1, ispecParams);
-
-
-let resultMSE0 = mse(calcISTFT0.slice(0, channel0.length-1), channel0);
-let resultMSE1 = mse(calcISTFT1.slice(0, channel1.length-1), channel1);
-console.log('Channel 0 data sets are different by ' + resultMSE0);
-console.log('Channel 1 data sets are different by ' + resultMSE1);
-
-let output = new Float32Array(calcISTFT0, calcISTFT1)
-
-// Create WaveFileAgain
-let wav = new WaveFile();
+let output = new Float32Array(result0, result1)
+let wav = new wv.WaveFile();
 wav.fromScratch(2, 44100, '32f',
     output);
 
-fs.writeFileSync('tst2.wav', wav.toBuffer());
+fs.writeFileSync('testKoekestra.wav', wav.toBuffer());
 
+/**
+ *  A function that applies TF's STFT and Magenta's ISTFT to a single Float32Array (a channel)
+ * @param {*} channel
+ * @returns A Float32Array that should be similar to the original channel
+ */
+function preprocessing(channel){
+    console.log("Length of input: " + channel.length)
+    
+    const input = tf.tensor1d(channel, 'float32') // Here there's a bug that makes the array lose precision
+    let resultSTFT = tf.signal.stft(input, FRAME_LENGTH, HOP_LENGTH);
+    
+    let reImArray = interleaveReIm(tf.real(resultSTFT), tf.imag(resultSTFT))
+    console.log("Length after interleave: ", reImArray.length, reImArray[0].length)
+    let resultISTFT = istft(reImArray, ispecParams);
+    console.log("Length after ISTFT: ", resultISTFT.length)
+
+    let resultMSE = mse(resultISTFT.slice(0, channel.length-1), channel);
+    console.log('Channel data sets are different by ' + resultMSE);
+
+    return resultISTFT
+}
 
 /**
  *
@@ -88,46 +77,19 @@ function decodeFile(arrayBuffer) {
 
             let channel0 = audioBuffer._channelData[0];
             let channel1 = audioBuffer._channelData[1];
+            console.log("\n------------------- Test with WavDecoder's Data ---------------------------\n")
+            console.log("\nProcessing channel 0\n")
+            const result0 = preprocessing(channel0)
+            console.log("\nProcessing channel 1\n")
+            const result1 = preprocessing(channel1)
 
-            //Crio o TF 1D
-            const input0 = tf.tensor1d(channel0, 'float32') // Here there's a bug that makes the array loses precision
-            const input1 = tf.tensor1d(channel1, 'float32') // Here there's a bug that makes the array loses precision
+            let output = new Float32Array(result0, result1)
 
-
-            // STFT
-            let result0 = tf.signal.stft(input0, FRAME_LENGTH, HOP_LENGTH);
-            let result1 = tf.signal.stft(input1, FRAME_LENGTH, HOP_LENGTH);
-
-            result0.print(true)
-
-            let real0 = tf.real(result0);
-            let real1 = tf.real(result1);
-
-            let imag0 = tf.imag(result0);
-            let imag1 = tf.imag(result1);
-
-            //Interleaved
-            let reIm0 = interleaveReIm(real0, imag0);
-            let reIm1 = interleaveReIm(real1, imag1);
-
-
-            let calcISTFT0 = istft(reIm0, ispecParams);
-            let calcISTFT1 = istft(reIm1, ispecParams);
-
-
-            let resultMSE0 = mse(calcISTFT0, channel0);
-            let resultMSE1 = mse(calcISTFT1, channel1);
-            console.log('Channel 0 data sets are different by ' + resultMSE0);
-            console.log('Channel 1 data sets are different by ' + resultMSE1);
-
-            let output = new Float32Array(calcISTFT0, calcISTFT1)
-
-            // Create WaveFileAgain
-            let wav = new WaveFile();
+            let wav = new wv.WaveFile();
             wav.fromScratch(2, 44100, '32f',
                 output);
 
-            fs.writeFileSync('tst.wav', wav.toBuffer());
+            fs.writeFileSync('testWavDecoder.wav', wav.toBuffer());
 
         } catch (e) {
             console.log(e)
@@ -184,7 +146,6 @@ function padConstant(data, padding) {
 function padCenterToLength(data, length) {
     // If data is longer than length, error!
     if (data.length > length) {
-        console.log(data.length, length)
         throw new Error('Data is longer than length.');
     }
 
@@ -193,47 +154,6 @@ function padCenterToLength(data, length) {
     return padConstant(data, [paddingLeft, paddingRight]);
 }
 
-// function padReflect(data: Float32Array, padding: number) {
-function padReflect(data, padding) {
-    const out = padConstant(data, padding);
-    for (let i = 0; i < padding; i++) {
-        // Pad the beginning with reflected values.
-        out[i] = out[2 * padding - i];
-        // Pad the end with reflected values.
-        out[out.length - i - 1] = out[out.length - 2 * padding + i - 1];
-    }
-    return out;
-}
-
-/**
- * Given a timeseries, returns an array of timeseries that are windowed
- * according to the params specified.
- */
-//function frame(data: Float32Array, frameLength: number, hopLength: number): Float32Array[] {
-function frame(data, frameLength, hopLength) {
-    const bufferCount = Math.floor((data.length - frameLength) / hopLength) + 1;
-    const buffers = Array.from({length: bufferCount}, (x, i) => new Float32Array(frameLength));
-    for (let i = 0; i < bufferCount; i++) {
-        const ind = i * hopLength;
-        const buffer = data.slice(ind, ind + frameLength);
-        buffers[i].set(buffer);
-        // In the end, we will likely have an incomplete buffer, which we should
-        // just ignore.
-        if (buffer.length !== frameLength) {
-            continue;
-        }
-    }
-    return buffers;
-}
-
-// function fft(y: Float32Array) {
-function fft(y) {
-    const fft = new FFT(y.length);
-    const out = fft.createComplexArray();
-    const data = fft.toComplexArray(y);
-    fft.transform(out, data);
-    return out;
-}
 
 // export function applyWindow(buffer: Float32Array, win: Float32Array) {
 function applyWindow(buffer, win) {
@@ -248,59 +168,6 @@ function applyWindow(buffer, win) {
         out[i] = win[i] * buffer[i];
     }
     return out;
-}
-
-
-//function stft(y: Float32Array, params: SpecParams): Float32Array[] {
-/**
- *
- * @param y
- * @param params
- *
-  sampleRate: number;
-  hopLength?: number;
-  winLength?: number;
-  nFft?: number;
-  nMels?: number;
-  power?: number;
-  fMin?: number;
-  fMax?: number;
- * @returns {[]}
- */
-function stft(y, params) {
-    const nFft = params.nFft || 2048;
-    const winLength = params.winLength || nFft;
-    const hopLength = params.hopLength || Math.floor(winLength / 4);
-
-    let fftWindow = hannWindow(winLength);
-
-    // Pad the window to be the size of nFft.
-    fftWindow = padCenterToLength(fftWindow, nFft);
-
-    // Pad the time series so that the frames are centered.
-    y = padReflect(y, Math.floor(nFft / 2));
-
-    // Window the time series.
-    const yFrames = frame(y, nFft, hopLength);
-    // Pre-allocate the STFT matrix.
-    const stftMatrix = [];
-
-    const width = yFrames.length;
-    const height = nFft + 2;
-    for (let i = 0; i < width; i++) {
-        // Each column is a Float32Array of size height.
-        const col = new Float32Array(height);
-        stftMatrix[i] = col;
-    }
-
-    for (let i = 0; i < width; i++) {
-        // Populate the STFT matrix.
-        const winBuffer = applyWindow(yFrames[i], fftWindow);
-        const col = fft(winBuffer);
-        stftMatrix[i].set(col.slice(0, height));
-    }
-
-    return stftMatrix;
 }
 
 // export function ifft(reIm: Float32Array): Float32Array {
@@ -323,7 +190,6 @@ function ifft(reIm) {
     const result = fft.fromComplexArray(recon);
     return result;
 }
-
 
 //function istft(reIm: Float32Array[], params: InverseSpecParams): Float32Array {
 /**
@@ -416,7 +282,7 @@ function interleaveReIm(real, imag) {
     let realArray = real.arraySync();
     let imagArray = imag.arraySync();
 
-    console.log(realArray.length, realArray[0].length);
+    console.log("Lenght at interleave: " + realArray.length, realArray[0].length);
 
     const resInterleaved = new Array();
 
@@ -431,11 +297,6 @@ function interleaveReIm(real, imag) {
         }
         resInterleaved.push(frame)
     }
-
-    // console.log(resInterleaved, resInterleaved.length, resInterleaved[0].length)
-
-
-    console.log("Results: " + resInterleaved[0].slice(0, 30), resInterleaved.length, resInterleaved[0].length);
 
     return resInterleaved
 }
