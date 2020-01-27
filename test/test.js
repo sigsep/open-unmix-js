5da03e5f647b9525259e0a49894259a2221b1ab0
Clara Jacintho <mcmachry@gmail.com>
2020-01-27 14:36:01 +0100

    added package.json, added moccha for test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+// STFT  test
+// 1. Simple array
+// 2. Compare with python output => save py output in float archive? 
+
+// ISTFT test
+// 1. Simple array
+// 2. Get the song back from the STFT
+ 
