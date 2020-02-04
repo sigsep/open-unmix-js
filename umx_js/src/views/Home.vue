91740d752b7f109add60f560998dd6c1a835c660
Clara Jacintho <mcmachry@gmail.com>
2020-02-04 12:47:55 +0100

    added drag and drop, started work on wavesurfer

--- umx_js/src/views/Home.vue
+++ umx_js/src/views/Home.vue
@@ -1,18 +1,117 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <Loader/>
-  </div>
+  <v-app id='app' :dark="dark">
+  <v-container>
+
+    <vue-dropzone 
+      id="drop" 
+      :options="dropOptions"
+      @vdropzone-complete="loadFile"
+    ></vue-dropzone>
+
+    <vue-wave-surfer :src="file" :options="waveSurferOptions"></vue-wave-surfer>
+    
+    <v-row no-gutters>
+        <v-select v-if="tracks.length > 1"
+          :dark="dark"
+          v-model="selectedTrack"
+          class="select"
+          :items="tracks"
+          light
+          label="Select track to separate"
+        ></v-select>
+      <Player :ref="player" :urls="tracklist" :dark="dark"></Player>
+      <v-layout
+        align-center
+        justify-center
+        style="background: red;"
+      >
+      </v-layout>
+    </v-row>
+
+  </v-container>    
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import Loader from '@/components/Loader.vue'
+import vueDropzone from "vue2-dropzone";
+import Player from './../components/Player.vue'
+import axios from 'axios'
+import VueWaveSurfer from 'vue-wave-surfer'
 
 export default {
-  name: 'home',
-  components: {
-    Loader
+  name: 'Home',
+  components: { Player, vueDropzone, VueWaveSurfer },
+  data () {
+    return {
+      dropOptions: {
+        url: "https://httpbin.org/post",
+        maxFilesize: 5, // MB
+        maxFiles: 1,
+        //chunking: true,
+        //chunkSize: 500, // Bytes
+        thumbnailWidth: 150, // px
+        thumbnailHeight: 150,
+        addRemoveLinks: true
+      },
+      waveSurferOptions: {
+
+      },
+      file: 'file://C:\\Users\\Clara\\Documents\\Polytech\\IG5_9\\pfe\\umx.js-pfe\\data\\audio_example.mp3',
+      dark: false,
+      tracks: [],
+      stems: [],
+      selectedTrack: '',
+      baseUrl: process.env.BASE_URL
+    }
+  },
+  mounted: function () {
+    this.fetchData();
+  },
+  created: function () {
+    
+  },
+  methods: {
+    fetchData(){
+     axios.get(this.baseUrl + 'headers.json').then(response => {
+        this.tracks = response.data.tracks
+        this.stems = response.data.stems
+        this.selectedTrack = response.data.selected_track
+        this.dark = response.data.dark
+     })
+    },
+    loadFile: function(file) {
+      let blob = window.URL || window.webkitURL;
+      let fileURL = blob.createObjectURL(file);   
+      console.log(fileURL)  
+    }
+  },
+  computed: {
+    tracklist: function () {
+      var trackstoload = []
+      for (let stem of this.stems) {
+        trackstoload.push(
+            { 'name': stem,
+              'customClass': stem,
+              'solo': false,
+              'mute': false,
+              'src': [
+                'tracks', this.selectedTrack, stem
+              ].join('/') + '.m4a'
+          })
+      }
+      return trackstoload
+    }
   }
+
 }
 </script>
+
+<style lang="stylus">
+#app {
+   height: 100vh;
+}
+
+.select {
+  z-index: 1000
+}
+</style>
